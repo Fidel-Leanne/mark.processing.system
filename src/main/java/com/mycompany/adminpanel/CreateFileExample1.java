3bc62a637ef722daab372b24dfe7584b453ac0da
fidelia M <fidelialtm13@gmail.com>
2022-10-08 01:08:22 +0200

    result.system.java

---
+++ src/main/java/com/mycompany/adminpanel/CreateFileExample1.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.adminpanel;
+
+/**
+ *
+ * @author Fidelia
+ */
+import java.io.File;  
+import java.io.IOException;  
+public class CreateFileExample1   
+{  
+public static void main(String[] args)   
+{     
+File file = new File("lecturer_records.txt"); //initialize File object and passing path as argument  
+boolean result;  
+try   
+{  
+result = file.createNewFile();  //creates a new file  
+if(result)      // test if successfully created a new file  
+{  
+System.out.println("file created "+file.getCanonicalPath()); //returns the path string  
+}  
+else  
+{  
+System.out.println("File already exist at location: "+file.getCanonicalPath());  
+}  
+}   
+catch (IOException e)   
+{  
+e.printStackTrace();    //prints exception if any  
+}         
+}  
+}  
